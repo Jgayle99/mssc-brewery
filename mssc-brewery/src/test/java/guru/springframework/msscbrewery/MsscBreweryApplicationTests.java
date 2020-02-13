0a84cb65d7e6abec6c1e6b41de44c3e44e6cbc4a
Jgayle <Jgayle99@gmail.com>
2020-02-12 21:24:16 -0500

    Added RequestBody annotations and test class for BeerController

---
+++ mssc-brewery/src/test/java/guru/springframework/msscbrewery/MsscBreweryApplicationTests.java
@@ -0,0 +1,15 @@
+package guru.springframework.msscbrewery;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@SpringBootTest
+class MsscBreweryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
