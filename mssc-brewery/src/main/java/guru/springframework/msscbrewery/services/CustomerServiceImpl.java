b8dbc1c5009e6b6e15ea9a8457d5c025259ee0d3
Jgayle <Jgayle99@gmail.com>
2020-02-12 21:43:55 -0500

    Added methods for Customer

--- mssc-brewery/src/main/java/guru/springframework/msscbrewery/services/CustomerServiceImpl.java
+++ mssc-brewery/src/main/java/guru/springframework/msscbrewery/services/CustomerServiceImpl.java
@@ -2,6 +2,7 @@ package guru.springframework.msscbrewery.services;
 
 import guru.springframework.msscbrewery.web.model.BeerDto;
 import guru.springframework.msscbrewery.web.model.CustomerDto;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.UUID;
@@ -9,6 +10,7 @@ import java.util.UUID;
 /**
  * Created by Joel Gayle on 12 Feb, 2020
  */
+@Slf4j
 @Service
 public class CustomerServiceImpl implements CustomerService {
     @Override
@@ -16,5 +18,23 @@ public class CustomerServiceImpl implements CustomerService {
         return CustomerDto.builder().id(UUID.randomUUID())
                 .name("Joel")
                 .build();
+    }
+
+    @Override
+    public CustomerDto saveNewCustomer(CustomerDto customerDto) {
+        return CustomerDto.builder()
+                .id(UUID.randomUUID())
+                .build();
+    }
+
+    @Override
+    public void updateCustomer(UUID customerId, CustomerDto customerDto){
+        //todo impl would add a real impl to update customer
+        log.debug("Updating a customer...");
+    }
+
+    @Override
+    public void deleteById(UUID customerId) {
+        log.debug("Deleting a customer...");
     }
 }
